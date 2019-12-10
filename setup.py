4951b40a952b554afa78d71e30f53e1e3763483c
Faremir <j.faremir.b@gmail.com>
2019-12-10 02:11:56 +0100

    -- version update

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from setuptools import setup
 
-setup(
-        name = 'AoC',
-        version = '1.19.05.1,
-        url = 'https://jircode.com',
-        license = 'GNU 3',
-        author = 'faremir',
-        author_email = 'faremir@jircode.com',
-        description = 'Advent of Code solutions',
-        packages=['Y19', 'Y19', 'utils']
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setup(name = 'AoC',
+      version = '1.19.05.1',
+      url = 'https://jircode.com',
+      license = 'GNU 3',
+      author = 'faremir',
+      author_email = 'faremir@jircode.com',
+      description = 'Advent of Code solutions',
 
-        )
+      long_description = long_description,
+      long_description_content_type = "text/markdown",
+      packages = ['Y19', 'Y19', 'utils']
+      )
