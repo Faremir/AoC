bc9a49f64e0c946f640008f38aca316531ee295d
Jiří Blahút <j.faremir.b@gmail.com>
2023-12-18 15:21:02 +0100

    [CORE][REF]

--- setup.py
+++ setup.py
@@ -3,16 +3,14 @@ from setuptools import setup
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setup(name = 'AoC',
-      version = '1.19.05.1',
-      url = 'https://jircode.com',
-      license = 'GNU 3',
-      author = 'faremir',
-      author_email = 'faremir@jircode.com',
-      description = 'Advent of Code solutions',
-
-      long_description = long_description,
-      long_description_content_type = "text/markdown",
-      packages = ['Y19', 'Y19', 'utils'],
-      python_requires = '>=3.7',
-      )
+setup(
+    name="AoC",
+    version="2023.0.0.0",
+    license="GNU 3",
+    author="Faremir",
+    author_email="j.faremir.b@gmail.com",
+    description="Advent of Code solutions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.11",
+)
